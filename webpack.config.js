f9a15c8f3bfa8fe25eac9b4e59dc8a34e1b39260
Miguel Angel <me@devmiguelangel.com>
2019-11-18 11:41:46 -0400

    01. webpack config and multiple entries

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: {
+    home: path.resolve(__dirname, "src/js/index.js"),
+    prices: path.resolve(__dirname, "src/js/prices.js"),
+    contact: path.resolve(__dirname, "src/js/contact.js")
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css?$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
